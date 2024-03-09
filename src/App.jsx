836ffa6cf568167a730b109a9f95aae16ddaa5e2
Zain Riaz <149959596+ZainRiaz09@users.noreply.github.com>
2024-03-09 19:02:03 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Zain from "./zain"
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+ <Zain/>
+ <h1>chai or react</h1>
+ </>
+  )
+}
+
+export default App
---
+++ src/zain.jsx
@@ -0,0 +1,6 @@
+function Zain(){
+    return(
+        <h3>first react</h3>
+    )
+}
+export default Zain
